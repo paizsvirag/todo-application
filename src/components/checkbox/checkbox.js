16c8dd0bec8a1d69e5909a2a9f885a7bea7ab96c
paizsvirag <paizs.virag@gmail.com>
2021-04-07 13:52:43 +0200

    --

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -13,7 +13,6 @@ export default function Checkbox({disabled = false,checked,
             disabled={disabled}
             checked={checked}
             onChange={handler}
-            area-labelledby="checkbox-label"
         />
     )
 }
