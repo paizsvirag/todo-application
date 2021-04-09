fb52fcca9cfc2492dfcc251a21894b9569d6345d
paizsvirag <paizs.virag@gmail.com>
2021-04-09 10:44:29 +0200

    data back css comp err

--- src/components/todo-list/todoList.js
+++ src/components/todo-list/todoList.js
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoItem from '../todo-item/todoItem';
+import './todoList.css';
+import * as data from './../../data/data.json' 
 
 export default function TodoList() {
     //TODO: todo list
     const [title, setTitle] = useState('');
     const updateTitle = () => setTitle(title);
+    const [todoItems, setTodoItems] = useState([]);
 
+    
+
+    //const deleteItem = () => 
     //TODO: list out todo items
-    return(<div>
-        <TodoItem />
+    return(<div className="todo-list-wrapper">
+        {data.map((element) => {
+            if(element.done) {
+                return (
+                <div className="todo-list-item-wrapper">
+                    <TodoItem />
+                    <button className="delete-button" >&times;</button>
+                </div>)
+            };
+            return;
+        })}
         <input
             className="todo-title-input"
             type="text"
