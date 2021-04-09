ce753a932b7c6907c9597f9838bab51987d5c056
paizsvirag <paizs.virag@gmail.com>
2021-04-09 14:00:41 +0200

    changes

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-function Checkbox({disabled = false,checked,
-    onChange}) {
+function Checkbox({ disabled = false, checked, onChange }) {
+  const handler = !disabled ? onChange : undefined;
 
-    const handler = !disabled ? onChange : undefined;
-
-    return(
-        <input
-            className="checkbox-input"
-            type="checkbox"
-            disabled={disabled}
-            checked={checked}
-            onChange={handler}
-        />
-    );
-};
+  return (
+    <input
+      className="checkbox-input"
+      type="checkbox"
+      disabled={disabled}
+      checked={checked}
+      onChange={handler}
+    />
+  );
+}
 
 export default Checkbox;
--- src/components/todo-list/todoList.js
+++ src/components/todo-list/todoList.js
@@ -1,40 +1,44 @@
 import React, { useState, useEffect } from "react";
-import TodoItem from '../todo-item/todoItem';
-import './todoList.css';
-import * as data from './../../data/data.json' 
+import TodoItem from "../todo-item/todoItem";
+import "./todoList.scss";
+import data from "./../../data/data.json";
 
 export default function TodoList() {
-    const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+  
+  useEffect(function updateDataToTodoItems() {
+      setTodoItems(data);
+    }, []);
+    
     const updateTitle = () => setTitle(title);
-    const [todoItems, setTodoItems] = useState([]);
-
-    useEffect( function updateDataToTodoItems(){
-        setTodoItems(data);
-    },[] )
 
     const deleteTodoItem = (id) => {
-        const newTodoItems = todoItems.default.filter()
-    }
+        //const newTodoItems = todoItems.filter()
+    };
+    
+  const addTodoItem = () => {};
 
-    const addTodoItem = () => {
-         
-    }
-
-    return(<div className="todo-list-wrapper">
-        {todoItems.default ? todoItems.default.map((element) => {
-            if(element.done) {
-                return (
-                <div id={element.id} className="todo-list-item-wrapper">
-                    <TodoItem title={element.todoTitle}/>
-                    <button className="delete-button" onClick={deleteTodoItem(element.id)}>&times;</button>
-                </div>)
-            };
-            return (<div><p>There is no todo for today! Yay!</p></div>);
-        }): <div></div>}
-        <input
-            className="todo-title-input"
-            type="text"
-            onChange={updateTitle}
-        />
-    </div>)
+  return (
+    <div className="todo-list-wrapper">
+      {todoItems ? (
+        todoItems.map((element) => {
+          return (
+            <div id={element.id} className="todo-list-item-wrapper">
+              <TodoItem title={element.todoTitle} />
+              <button
+                className="delete-button"
+                onClick={deleteTodoItem(element.id)}
+              >
+                &times;
+              </button>
+            </div>
+          );
+        })
+      ) : (
+        <div></div>
+      )}
+      <input className="todo-title-input" type="text" onChange={updateTitle} />
+    </div>
+  );
 }
